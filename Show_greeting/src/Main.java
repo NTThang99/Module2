98c28298e5b166ba97f9f489bf5e57f687c1f7d4
TrongThang <quandh@deha-soft.com>
2023-05-09 16:22:41 +0700

    up

---
+++ Show_greeting/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner BC = new Scanner(System.in);
+        System.out.println("Nhập tên của bạn:");
+        String name = BC.nextLine();
+        System.out.println("Hello " + name);
+        }
+    }
